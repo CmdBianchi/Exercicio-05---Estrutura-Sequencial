231611266aa539b1c621289f08d6b8d001ae94e9
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-21 15:05:39 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio-05 - Estrutura-Sequencial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_05___Estrutura_Sequencial
+{
+    class Program
+    {
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Msg();
+            Leitura();
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg() {
+            Console.WriteLine("Calcule e mostre o valor a ser pago da peça 1 e da peça 2");
+            Console.WriteLine(" ");
+        }
+        /*------------------------------------------------------------------------------------------*/
+        static void Leitura() {
+            Console.WriteLine("Escreva o código do produto 1: ");
+            Console.WriteLine("Escreva o código do produto 2: ");
+            Console.WriteLine(" ");
+        }
+
+
+
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+    }
+}
